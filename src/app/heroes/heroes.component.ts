5feeb2e2f9756420002ba36b690e44a77ff0b7e4
Emily <sheil.emily.sc@gmail.com>
2023-04-27 14:51:54 +0200

    services

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,21 +1,42 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from '../mock-heroes';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-export class HeroesComponent {
+export class HeroesComponent implements OnInit {
   // hero: Hero = {
   //   id: 1,
   //   name: 'Windstorm'
   // };
-  heroes = HEROES;
+  heroes: Hero[] = [];
   selectedHero?: Hero;
+
+  // defines private property 'heroService' and makes it the site of the injection of HeroService
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit(): void {
+    this.getHeroes()
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`)
   }
+
+  // SYNCHRONOUS version only works if server can instantaneously return value (as if the browser can freeze the UI while waiting for a response)
+  // getHeroes(): void {
+  //   this.heroes = this.heroService.getHeroes();
+  // }
+
+  // ASYNC version with observable waits for the observable to deliver the requested data. subscribe() passes value to callback i.e. setting component's heroes property
+  getHeroes(): void {
+    this.heroService.getHeroes()
+      .subscribe(heroes => this.heroes = heroes)
+  }
+
 }
